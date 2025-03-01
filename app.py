8f99f233c966c6c1ead6ca0a1c7235d948dec181
gregbkatz <gregbkatz@gmail.com>
2025-03-01 14:59:59 -0800

    Updates for render.com

--- app.py
+++ app.py
@@ -302,4 +302,5 @@ def manage_bracket():
     return jsonify(bracket)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001) 
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port) 
