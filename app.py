ce4274b581bca23d719f41655b14d52cd4ebdafa
gregbkatz <gregbkatz@gmail.com>
2025-03-15 13:27:32 -0700

    Change to port 80

--- app.py
+++ app.py
@@ -538,5 +538,5 @@ def manage_bracket():
     return jsonify(user_bracket)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port) 
