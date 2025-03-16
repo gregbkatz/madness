196f47790c65f157aa9cec30fc53eb11598b6036
gregbkatz <gregbkatz@gmail.com>
2025-03-16 14:38:06 -0700

    use waitress

--- restart_flask.sh
+++ restart_flask.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until python app.py; do
+until waitress-serve --host=0.0.0.0 --port=80 app:app; do
     echo "'app.py' crashed with exit code $?. Restarting..." >&2
     sleep 1
 done
