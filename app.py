49f272c773f733983eec5318574fc443af61b706
gregbkatz <gregbkatz@gmail.com>
2025-03-20 09:26:12 -0700

    Switch back to port 80

--- app.py
+++ app.py
@@ -1068,5 +1068,5 @@ def users_list():
         return render_template('users_list.html', users=[], error=str(e))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port) 
