1f67d6c70a73fb7e3ab5959b202415ac84d1cbda
gregbkatz <gregbkatz@gmail.com>
2025-03-27 19:28:32 -0700

    Update truth

--- generate_all_simulations.py
+++ generate_all_simulations.py
@@ -23,7 +23,7 @@ def parse_arguments():
     parser.add_argument(
         '--count', 
         type=int, 
-        default=1000,
+        default=20000,
         help='Number of simulations to generate for each truth bracket (default: 1000)'
     )
     parser.add_argument(
